c46ef64d01c3658ef645327931e78b3eb42b59b9
Nb-aris <bienvenuearistide@gmail.com>
2022-08-05 13:16:41 +0200

    third commit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import AddressDisplay from './AddressDisplay';
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Banner bannerText="Sextant GUI" />
-                <Exhibit name="Exhibit1"></Exhibit>
-                <Exhibit name="Exhibit2"></Exhibit>
-                <Exhibit name="Exhibit3"></Exhibit>
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+				</Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+				</Exhibit>
             </div>
         );
     }
